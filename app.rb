e428c0e077f59899a5bbe30bd0b23bd347352304
Vasu Adari <vasuakeel@gmail.com>
2020-01-13 14:28:31 +0530

    Enable logging in production and development environments

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ class App < Sinatra::Base
 
   configure { set :server, :puma }
 
+  configure :production, :development do
+    enable :logging
+  end
+
   get '/hello-world' do
     'hello world!'
   end
